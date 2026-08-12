fecccc49b798b9d0be42731ea602c2ed51f6be2b
Donkazuya <danrley.tri@gmail.com>
2026-08-12 13:51:12 -0300

    Fix: icons

--- calcula-compras/src/app/app.config.ts
+++ calcula-compras/src/app/app.config.ts
@@ -4,9 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideNgxMask } from 'ngx-mask';
 import { provideHttpClient } from '@angular/common/http';
-import {  tuiAssetsPathProvider} from '@taiga-ui/core';
-import {NG_EVENT_PLUGINS} from '@taiga-ui/event-plugins';
-import {provideAnimations} from '@angular/platform-browser/animations';
+import { tuiAssetsPathProvider } from '@taiga-ui/core';
+import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,9 +15,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideNgxMask(),
-    tuiAssetsPathProvider('https://taiga-ui.dev/assets/taiga-ui/icons'),
+    tuiAssetsPathProvider('/assets/taiga-ui/icons'),
     NG_EVENT_PLUGINS,
     provideAnimations(),
-    {provide: 'LOCALE_ID', useValue: 'pt-BR'}
+    { provide: 'LOCALE_ID', useValue: 'pt-BR' }
   ]
 };
